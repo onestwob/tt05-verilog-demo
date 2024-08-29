7954fb82e0403bf5ea0b3596e7eaca74b28fad58
onestwob <onestwob@gmail.com>
2024-08-29 13:57:48 +0900

    Update test.py
    Use tt04's test.py

--- src/test.py
+++ src/test.py
@@ -2,10 +2,6 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
-
 
 segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
 
